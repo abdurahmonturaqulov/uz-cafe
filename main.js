8a95fbff53798a23820daa9affddd9529cf6d905
Abdurahmon <rahmlow.1217@mail.ru>
2025-04-13 07:56:40 +0500

    2file adds

--- main.js
+++ main.js
@@ -3,9 +3,18 @@ const app=Vue.createApp({
         return{
             name:"murod",
             job: "developer",
-            age: 35
+            age: 35,
+            showAge:true
         }
+    },
+
+   methods: {
+    showOrHideAge(){
+        this.showAge=!this.showAge
     }
+   }
+
+   
 })
 
 app.mount('#app');
